4c690135c372c0d6388fe71e4488d5382fbcb0d0
Selety <110613888+MohamedSelety@users.noreply.github.com>
2022-09-14 03:08:04 +0200

    first commit

---
+++ src/test/java/stepDefinitions/LoginStepDefinition.java
@@ -0,0 +1,46 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import pages.HomePage;
+import pages.LoginPage;
+import static stepDefinitions.Hooks.driver;
+
+public class LoginStepDefinition {
+    HomePage homePage;
+    LoginPage loginPage;
+
+    @And("user clicked on login link")
+    public void userClickedOnLoginLink() {
+        homePage = new HomePage(driver);
+        homePage.loginClick();
+    }
+
+    @And("user entered email for login")
+    public void userEnteredEmailForLogin() {
+        loginPage = new LoginPage(driver);
+        loginPage.enterEmail("midoselety5@gmail.com");
+    }
+
+    @And("user entered password for login")
+    public void userEnteredPasswordForLogin() {
+        loginPage.enterPassword("Moselety");
+    }
+
+    @And("user clicked on login button")
+    public void userClickedOnLoginButton() throws InterruptedException {
+        loginPage.clickLogIn();
+
+            Thread.sleep(3000);
+
+    }
+
+    @Then("user should have a success message for login")
+    public void userShouldHaveASuccessMessageForLogin() {
+
+        Assert.assertTrue(driver.findElement(By.xpath("//a[@class=\"ico-account\"]")).isDisplayed());
+        Assert.assertEquals(driver.findElement(By.xpath("//a[@class=\"ico-account\"]")).getText(), "My account");
+    }
+}
